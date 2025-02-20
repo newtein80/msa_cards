ce2a9c861770f883a0041aeb86a5b9b982bdf635
newtein80 <newtein80@gmail.com>
2025-02-20 19:43:56 +0900

    first commit

---
+++ src/main/java/com/mobigen/cards/CardsApplication.java
@@ -0,0 +1,43 @@
+package com.mobigen.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@EnableDiscoveryClient
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Cards microservice REST API Documentation",
+		description = "MSA - Cards microservice REST API Documentation",
+		version = "v1",
+		contact = @Contact(
+			name = "group1-team2",
+			email = "group1-team2@email.com",
+			url = "https://www.group1-team2.com"
+		),
+		license = @License(
+			name = "Apache 2.0",
+			url = "https://www.group1-team2.com"
+		)
+	),
+	externalDocs = @ExternalDocumentation(
+		description =  "MSA - Cards microservice REST API Documentation",
+		url = "https://ip:port/swagger-ui.html"
+	)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
